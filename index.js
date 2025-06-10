2ef6a575dfe1dea64c0e715f7c4eeeb68954511f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2025-06-11 00:52:49 +0800

    code clean-up

--- index.js
+++ index.js
@@ -4,49 +4,55 @@ const app = express();
 const port = 8500;
 
 app.use(express.static("public"));
-app.use(express.static("publiko"));
-
-const todosRouter = express.Router();
-
-// reading all todo items
-todosRouter.get("/todos", (req, res) => {
-  res.send("Get All TODOS");
-});
 
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
+
+// authentication routes
+app.get("/auth/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/auth/registration", (req, res) => {
+  res.render("auth/registration");
+});
+
+// user routes
+app.get("/user/:id", (req, res) => {
+  const userId = req.params.id;
+
+  // TODO: fetched from the database
+
+  // TODO: fetch all todo from user
+
+  res.render("user/user");
+});
+
+// api routes
+app.post("/api/todo", (req, res) => {
+  // TODO: create a new todo item
+
+  res.json({ message: "Create todo successful" });
+});
+
+app.put("/api/todo/:id", (req, res) => {
+  const todoId = req.params.id;
+
+  // TODO: update the todo item with the given id
+
+  res.json({ message: `Update todo with id ${todoId} successful` });
+});
+
+app.delete("/api/todo/:id", (req, res) => {
+  const todoId = req.params.id;
+
+  res.json({ message: `Delete todo with id ${todoId} successful` });
+});
 
 // Define a basic route
 app.get("/", (req, res) => {
   res.render("index", { title: "EJS Example", message: "Welcome to EJS!" });
 });
-
-app.get("/users", (req, res) => {
-  // fetched from the database
-  const user1 = { name: "Alice", age: 18 };
-  const user2 = { name: "Bob", age: 30 };
-  const user3 = { name: "Charlie", age: 32 };
-
-  const users = [user1, user2, user3];
-  res.render("users", { users: [], test: "This is a test" });
-});
-
-app.get("/user/:id", (req, res) => {
-  const userId = req.params.id;
-
-  const user1 = { id: "user1", name: "Alice", age: 18 };
-  const user2 = { id: "user2", name: "Bob", age: 30 };
-  const user3 = { id: "user3", name: "Charlie", age: 32 };
-
-  const users = [user1, user2, user3];
-  res.render("user", { users: users, id: userId });
-});
-
-app.get("/sign-in", (req, res) => {
-  res.send("Login screen");
-});
-
-app.use("/", todosRouter);
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
