e7ffc9a55faba5981184b5e55e3c302f91161cf9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2025-06-24 23:40:27 +0800

    Added todo

--- routes/todos/routes.js
+++ routes/todos/routes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import redirectIfNotAuthenticated from "../../middlewares/redirectIfNotAuthenticated.js";
 
+import Todo from "../../models/todo.js";
+
 const todosRouter = express.Router();
 
 todosRouter.use(redirectIfNotAuthenticated);
 
 todosRouter.get("/", (req, res) => {
   // get the user id from the session
-  const { name } = req.session.user;
+  const { id, name } = req.session.user;
+
+  // TODO: get all todos that belongs to the user
 
   res.render("todos/index", { name, authenticated: true });
 });
