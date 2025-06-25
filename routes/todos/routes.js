b1feec020ef2ca7cb5d7a316ffc6b0d8445c998a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2025-06-25 10:40:52 +0800

    finished search

--- routes/todos/routes.js
+++ routes/todos/routes.js
@@ -8,20 +8,33 @@ const todosRouter = express.Router();
 todosRouter.use(redirectIfNotAuthenticated);
 
 todosRouter.get("/", async (req, res) => {
+  const search = req.query.search || "";
+
   // get the user id from the session
   const { id, name } = req.session.user;
 
   let todos = [];
 
   try {
-    // get all todos that belongs to the user
-    todos = await Todo.find({ userId: id });
+    if (search) {
+      // query the todos that match the search term
+      todos = await Todo.find({
+        userId: id,
+        $or: [
+          { title: { $regex: search, $options: "i" } }, // case-insensitive search for title
+          { description: { $regex: search, $options: "i" } },
+        ],
+      });
+    } else {
+      // get all todos that belongs to the user
+      todos = await Todo.find({ userId: id });
+    }
   } catch (error) {
     // something went wrong, log the error
     console.error(error);
   }
 
-  res.render("todos/index", { name, authenticated: true, todos });
+  res.render("todos/index", { name, authenticated: true, todos, search });
 });
 
 export default todosRouter;
